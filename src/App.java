c53e9fb185962041410de1bf7c1cc008090f5113
Daniel <daniel@Clouds-MacBook-Pro.local>
2024-06-08 17:45:32 -0400

    test

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        //hello!!!!!!!
         Scanner a = new Scanner(System.in);
         String input, output;
         input = a.nextLine();
